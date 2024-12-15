0dae18b4c32da70549478a783029059ef3b4a548
18074 <proxypppp@qq.com>
2024-12-15 18:08:21 +0800

    init

--- src/main/java/com/ilzf/readerhelper/entity/BookEntity.java
+++ src/main/java/com/ilzf/readerhelper/entity/BookEntity.java
@@ -1,6 +1,8 @@
 package com.ilzf.readerhelper.entity;
 
+import cn.hutool.core.io.FileUtil;
 import lombok.Data;
+import lombok.val;
 
 import java.io.File;
 import java.util.List;
@@ -12,10 +14,13 @@ public class BookEntity {
     private String title;
     private String author;
     private String introduction;
+    private String size;
     private List<ChapterEntity> chapters;
 
     public static BookEntity init(File file) {
-        
-        return new BookEntity();
+        BookEntity bookEntity = new BookEntity();
+        bookEntity.setTitle(file.getName());
+        bookEntity.setSize(FileUtil.readableFileSize(file));
+        return bookEntity;
     }
 }
