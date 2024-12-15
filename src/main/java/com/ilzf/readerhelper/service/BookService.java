f2a2d03fdcaafa671a951eb5a5d72a0ec8a18b10
18074 <proxypppp@qq.com>
2024-12-15 20:11:03 +0800

    init

--- src/main/java/com/ilzf/readerhelper/service/BookService.java
+++ src/main/java/com/ilzf/readerhelper/service/BookService.java
@@ -8,6 +8,7 @@ import com.ilzf.readerhelper.config.ReaderPropertyConfig;
 import com.ilzf.readerhelper.constant.FileType;
 import com.ilzf.readerhelper.entity.BookEntity;
 import com.ilzf.readerhelper.entity.ChapterEntity;
+import com.ilzf.readerhelper.utils.TextBookUtil;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
@@ -15,15 +16,12 @@ import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class BookService {
     @Resource
     private ReaderPropertyConfig readerPropertyConfig;
 
-    private final Map<String, ChapterEntity> chapterEntityCache = new ConcurrentHashMap<>();
 
     public List<BookEntity> listBooks() {
         File booKPath = new File(readerPropertyConfig.getPath());
@@ -43,32 +41,7 @@ public class BookService {
             return result;
         }
         if (FileType.TXT == result.getFileType()) {
-            List<String> lines = FileUtil.readLines(file, StandardCharsets.UTF_8);
-            List<ChapterEntity> chapters = new ArrayList<>();
-            String content = "";
-            String title = "";
-            for (String line : lines) {
-                if (((line.startsWith("第") && (line.contains("章") || line.contains("回"))) || line.contains("创作手记") || line.contains("后记") || line.contains("楔子")) && line.length() < 30) {
-                    if (!StrUtil.isEmpty(content)) {
-                        if (StrUtil.isEmpty(title)) {
-                            String[] split = content.split("<br/>\n");
-                            if (split.length > 0 && split[0].length() < 30) {
-                                title = split[0];
-                            } else {
-                                title = "第%s章[自动]".formatted(chapters.size() + 1);
-                            }
-                        }
-                        setChapter(title, content, result, chapters);
-                    }
-                    title = line;
-                    content = "";
-                    continue;
-                }
-                if (StrUtil.isNotEmpty(line)) {
-                    content += line + "<br/>\n";
-                }
-            }
-            setChapter(title, content, result, chapters);
+            List<ChapterEntity> chapters = TextBookUtil.getChapterEntity(file, result);
             result.setChapters(chapters);
         }
 
@@ -79,14 +52,9 @@ public class BookService {
         return getDetail(path, true);
     }
 
-    private void setChapter(String title, String content, BookEntity result, List<ChapterEntity> chapters) {
-        ChapterEntity entity = ChapterEntity.builder().title(title).content(content).id(DigestUtil.md5Hex(title + result.getTitle())).build();
-        chapters.add(entity);
-        chapterEntityCache.put(entity.getId(), entity);
-    }
 
     public String getContent(String id) {
-        ChapterEntity entity = chapterEntityCache.get(id);
+        ChapterEntity entity = TextBookUtil.getChapterContent(id);
         if (entity == null) {
             return "";
         }
--- src/main/java/com/ilzf/readerhelper/utils/TextBookUtil.java
+++ src/main/java/com/ilzf/readerhelper/utils/TextBookUtil.java
@@ -1,4 +1,58 @@
 package com.ilzf.readerhelper.utils;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.digest.DigestUtil;
+import com.ilzf.readerhelper.entity.BookEntity;
+import com.ilzf.readerhelper.entity.ChapterEntity;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class TextBookUtil {
+    private static final Map<String, ChapterEntity> chapterEntityCache = new ConcurrentHashMap<>();
+
+    public static List<ChapterEntity> getChapterEntity(File file, BookEntity result) {
+        List<String> lines = FileUtil.readLines(file, StandardCharsets.UTF_8);
+        List<ChapterEntity> chapters = new ArrayList<>();
+        String content = "";
+        String title = "";
+        for (String line : lines) {
+            if (((line.startsWith("第") && (line.contains("章") || line.contains("回"))) || line.contains("创作手记") || line.contains("后记") || line.contains("楔子")) && line.length() < 30) {
+                if (!StrUtil.isEmpty(content)) {
+                    if (StrUtil.isEmpty(title)) {
+                        String[] split = content.split("<br/>\n");
+                        if (split.length > 0 && split[0].length() < 30) {
+                            title = split[0];
+                        } else {
+                            title = "第%s章[自动]".formatted(chapters.size() + 1);
+                        }
+                    }
+                    setChapter(title, content, result, chapters);
+                }
+                title = line;
+                content = "";
+                continue;
+            }
+            if (StrUtil.isNotEmpty(line)) {
+                content += line + "<br/>\n";
+            }
+        }
+        setChapter(title, content, result, chapters);
+        return chapters;
+    }
+
+    public static ChapterEntity getChapterContent(String id) {
+        return chapterEntityCache.get(id);
+    }
+
+    private static void setChapter(String title, String content, BookEntity result, List<ChapterEntity> chapters) {
+        ChapterEntity entity = ChapterEntity.builder().title(title).content(content).id(DigestUtil.md5Hex(title + result.getTitle())).build();
+        chapters.add(entity);
+        chapterEntityCache.put(entity.getId(), entity);
+    }
 }
