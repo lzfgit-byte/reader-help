939468d8eef9c63df7d3f16de3d7ab4782505ee3
18074 <proxypppp@qq.com>
2024-12-15 14:15:43 +0800

    init

---
+++ src/main/java/com/ilzf/reader_helper/ReaderHelperApplication.java
@@ -0,0 +1,13 @@
+package com.ilzf.reader_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilzf/reader_helper/ReaderHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilzf.reader_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
