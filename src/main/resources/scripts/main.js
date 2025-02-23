f8c7a115d7b5b84b2b02b745e158341c83580e3b
18074 <proxypppp@qq.com>
2025-02-23 17:32:18 +0800

    init

--- src/main/resources/scripts/main.js
+++ src/main/resources/scripts/main.js
@@ -1,4 +1,16 @@
 $(() => {
+  var table = layui.table;
+  table.init('demo', {
+    height: 600,
+    limit: 10
+  });
 
-  alert(1)
+
+  $('#addNew').click(() => {
+    layer.open({
+      title: '在线调试'
+      , content: $('#addNform')
+    });
+  })
+
 })
